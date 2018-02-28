5a3af661caff555169d996ca0032ab8d03cc4895
bot_3310 <jacky2001114@gmail.com>
2018-02-28 18:48:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         osu!-download-beatmap-from-bloodcat
 // @namespace    http://tampermonkey.net/
-// @version      0.7.1
+// @version      1.0.1
 // @description   download osu beatmap from bloodcat
 // @author       jacky2001114(blow)
 // @match        *://bloodcat.com/osu/?q=*&c=b&s=&m=*
@@ -10,6 +10,7 @@
 // @match        *://osu.ppy.sh/p/beatmap?b=*
 // @match        *://osu.ppy.sh/p/beatmap?s=*
 // @match        *://bloodcat.com/osu/*
+// @match        *://osu.ppy.sh/beatmapsets/*
 // @grant GM_setValue
 // @grant GM_getValue
 // ==/UserScript==
@@ -52,65 +53,20 @@
         var isS = str.indexOf('osu.ppy.sh/s');
         var isB = str.indexOf('osu.ppy.sh/b');
         var isP = str.indexOf('osu.ppy.sh/p');
+        var isBeatmapset = str.indexOf('osu.ppy.sh/beatmapsets');
         var eee=str.indexOf("?");
         var fff=str.indexOf("&");
         var isBeatmap =str.indexOf('beatmap');
 
 
         //check /s or /b
-        if(isS > -1 && isB===-1 && isP ===-1 )
+        if(isS != -1 || isB != -1 || isP !=-1 ||isBeatmapset != -1 || isBeatmap !=-1)
         {
-           //alert('s');
-            // the song url is /s
-            GM_setValue('beatmap_id',str.substring(21));
+
             //Get title
             getTitle();
 
-
-
-
-        }else if(isS===-1 && isB>-1 && isP ===-1){
-         
-//alert('b');
-            // the song url is /b
-
-            if(fff>-1){
-               
-                GM_setValue('beatmap_id',str.substring(21,fff));
-                var id = GM_getValue('beatmap_id');
-               //  alert(id+'1');
-                creatButton(id);
-            }else if(eee>-1){
-                GM_setValue('beatmap_id',str.substring(21,eee));
-                var id = GM_getValue('beatmap_id');
-              //  alert(id);
-                creatButton(id);
-            }else if(eee === -1&& fff===-1){
-                
-            GM_setValue('beatmap_id',str.substring(21));
-                var id = GM_getValue('beatmap_id');
-              //  alert(id);    
-                 creatButton(id);
-            } else{
-                alert('URL Error!Please report to the author with the URL');
-            }
-
-
-        }else if(isS===-1 && isB===-1 && isP >-1){
-         console.log('beat');
-            GM_setValue('beatmap_id',str.substring(31,fff));
-            var id = GM_getValue('beatmap_id');
-            creatButton(id);
-        }else {
-            alert('URL Error!Please report to the author with the URL');
         }
-
-
-
-
-        //alert(id);
-
-
 
 
     }
@@ -120,8 +76,19 @@
         var add_html ="<button id='blood_bt' style='float:left;top:0;width:180px;height:43px;background-color:red;color:white;'>bloodCat</button>";        
         var div = document.createElement("div");
         div.innerHTML = add_html;
-        document.getElementsByClassName('paddingboth')[0].append(div);
-        document.getElementById('blood_bt').addEventListener("click", function(){window.open("http://bloodcat.com/osu/?q="+id+"&c=b&s=&m=");});
+        if( document.getElementsByClassName('paddingboth')[0]!==undefined){
+            document.getElementsByClassName('paddingboth')[0].append(div);
+            document.getElementById('blood_bt').addEventListener("click", function(){window.open("http://bloodcat.com/osu/?q="+id+"&c=b&s=&m=");});
+        }
+        else{//find element every 1 second
+            var findElement= setInterval(()=>{
+
+                document.getElementsByClassName('beatmapset-header__buttons')[0].append(div);
+                document.getElementById('blood_bt').addEventListener("click", function(){window.open("http://bloodcat.com/osu/?q="+id+"&c=b&s=&m=");});
+                clearInterval(findElement);
+            },1000);
+
+        }
 
     }
 
@@ -130,10 +97,38 @@
 
     function getTitle(){
         //Get song name
-        var title=document.getElementById("songinfo").getElementsByClassName("colour")[3].getElementsByTagName("a")[0].innerHTML;
+        var artist,title,creator='';
+        var mix='';
+        if(isBeatmapset!=-1){
 
-        //creat a button element to bloodcat
-        creatButton(title);
+
+            var findElement= setInterval(()=>{
+
+                title =document.getElementsByClassName("beatmapset-header__details-text")[0].innerHTML;
+                artist =document.getElementsByClassName("beatmapset-header__details-text")[1].innerHTML;
+                creator=document.getElementsByClassName("beatmapset-mapping__user")[0].innerHTML;
+                if(title&&artist&&creator){
+
+                    mix=artist+' '+title+' '+creator;
+                    creatButton(mix);
+                    clearInterval(findElement);
+                }
+
+            },1000);
+
+
+
+
+        }else{
+
+            artist =document.getElementById("songinfo").getElementsByClassName("colour")[0].getElementsByTagName("a")[0].innerHTML;
+            title=document.getElementById("songinfo").getElementsByClassName("colour")[3].getElementsByTagName("a")[0].innerHTML;
+            creator=document.getElementById("songinfo").getElementsByClassName("colour")[6].getElementsByTagName("a")[0].innerHTML;
+            mix=artist+' '+title+' '+creator;
+            //creat a button element to bloodcat
+            creatButton(mix);
+        }
+
 
 
     }
@@ -154,32 +149,25 @@
     function verify(){
         //get length of sets
         var set_num = document.getElementsByClassName('set').length;
-        var sameBeatmap='';
-
-        var id = GM_getValue('beatmap_id');
+       
 
 
-        //alert(id);
+
+
+
         if(set_num===0){
             alert('The beatmap have not exist!');
 
-        }
-        //check same id
-        for(var i=0;i<set_num;i++){
-            sameBeatmap = document.getElementsByTagName('main')[0].getElementsByClassName('set')[i].getElementsByClassName('title')[0].getAttribute('href');
-            var cut=sameBeatmap.substring(2);
-            if(cut === id){
-                // alert("success");
-                document.getElementsByTagName('main')[0].getElementsByClassName('set')[i].getElementsByClassName('title')[0].click();
-            }
+        }else if(set_num==1){
+           document.getElementsByClassName('title')[1].click();
+        }else{
+
+            alert('more than 1 beatmap');
+
         }
 
-        if(str.indexOf(id)!== -1){
-            if(set_num==1)
-            {
-                document.getElementsByClassName('title')[1].click();
 
-            }
-        }
+
+
     }
 })();
